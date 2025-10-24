7ed646125896013e12a2d97ae782deca856a89e0
qadg <qadgmain@gmail.com>
2025-10-24 15:26:47 +0800

    修改 USER_INPUT_PROMPT 的 Question:

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ LORA_PATH = "results/checkpoint-233"
 INPUT_PATH = "working_dir/input.jsonl"
 OUTPUT_PATH = "working_dir/output.jsonl"
 
-USER_INPUT_PROMPT = """You are given a mathematical problem stated in natural language.  Your task is to translate it into a complete Litex formal solution, which includes both a `claim:` section stating the formal proposition and a `prove:` section providing a step-by-step logical derivation.
+# USER_INPUT_PROMPT = """You are given a mathematical problem stated in natural language.  Your task is to translate it into a complete Litex formal solution, which includes both a `claim:` section stating the formal proposition and a `prove:` section providing a step-by-step logical derivation.
 
-    Show each reasoning step clearly in the proof, and ensure the conclusion in the `claim:` is fully justified by the `prove:` section.
-    ### Problem
-    """# problem will be appended here
+#     Show each reasoning step clearly in the proof, and ensure the conclusion in the `claim:` is fully justified by the `prove:` section.
+#     ### Problem
+#     """# problem will be appended here
